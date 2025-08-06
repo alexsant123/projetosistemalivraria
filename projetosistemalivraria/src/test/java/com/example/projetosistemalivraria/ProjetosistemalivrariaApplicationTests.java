8e3a17877987094a3ce136f8eaa80218d9faaa28
alexsant123 <eua2016@hotmail.com>
2025-08-05 23:32:08 -0300

    Initial commit

---
+++ projetosistemalivraria/src/test/java/com/example/projetosistemalivraria/ProjetosistemalivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetosistemalivraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosistemalivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
